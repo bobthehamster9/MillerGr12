62af1dd4ccf93aba4a8028daefb2881d7a8f4e92
Jack Fountain <jack@rockyroost.com>
2019-12-18 22:14:18 -0500

    Finished GUI just need to finish CC check

--- src/me/Jack/Project.java
+++ src/me/Jack/Project.java
@@ -1,6 +1,7 @@
 package me.Jack;
 
 import me.Jack.Projects.ArrayLists.*;
+import me.Jack.Projects.GUI.Verification;
 import me.Jack.Review.*;
 
 import java.io.IOException;
@@ -8,7 +9,8 @@ import java.io.IOException;
 public class Project {
 
     public static void main(String[] args) throws IOException {
-
+        Verification  verification = new Verification();
+        verification.run();
     }
 
 }
